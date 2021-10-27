e77ef0a395a5c606866c1cdc8f7ab9d62e852bbf
Zili Huang <zili.huang@i22.de>
2021-10-27 15:25:07 +0300

    feat: enable eslint and prettier rules for typescript/nextjs changelog: transform js to ts/tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+
+function Portfolio({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default Portfolio;
